0826899da78f7a7974789ae532e72111d967d67b
Brandon <brandon.druschel@gmail.com>
2017-02-22 19:52:59 -0500

    Created FoodObject and its respective test class

---
+++ NutritionPlan/src/main/java/FoodObject.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class FoodObject {
+
+// Global variables
+	public String name, type; // name and type of food
+	public int cal; // calories
+	public ArrayList<String> alg; // list of ingredients concerning allergies
+
+// Constructor
+	public FoodObject() {
+		name = null;
+		type = null;
+		cal = 0;
+	}
+
+// Parameterized Constructor
+	public FoodObject(String n, String t, int c, ArrayList<String> a) {
+		name = n;
+		type = t;
+		cal = c;
+		alg = a;
+	}
+
+// Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getCal() {
+		return cal;
+	}
+
+	public void setCal(int cal) {
+		this.cal = cal;
+	}
+
+	public ArrayList<String> getAlg() {
+		return alg;
+	}
+
+	public void setAlg(ArrayList<String> alg) {
+		this.alg = alg;
+	}
+
+	
+// Methods
+	// check for valid calorie value
+	public boolean checkCal(int x) {
+		if (x < 0)
+			return false;
+		else
+			return true;
+	}
+
+	// check if food contains allergies
+	public boolean checkAlg() {
+		if (this.alg.isEmpty()) {
+			return false;
+		} else
+			return true;
+	}
+
+	// adjust total calories based on amount of servings
+	public void adjustCal(int s) {
+		this.setCal(cal *= s);
+	}
+
+}
--- NutritionPlan/src/test/java/FoodObjectTest.java
+++ NutritionPlan/src/test/java/FoodObjectTest.java
@@ -1,14 +1,43 @@
 import static org.junit.Assert.*;
+import java.util.*;
 
 import org.junit.Test;
 
 public class FoodObjectTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void checkCalTest() { //true if calories is nonnegative, false otherwise
+		ArrayList<String> allerg = new ArrayList<String>();
+		allerg.add("peanut");
+		FoodObject food = new FoodObject("Peanut Butter", "snack", 180, allerg);
+		FoodObject food2 = new FoodObject("Impossible Fruit", "fruit", -125, new ArrayList<String>());
 
+		assertTrue(food.checkCal(food.getCal()));
+		assertFalse(food.checkCal(food2.getCal()));
+	}
 	
-	//test comment
+	@Test
+	public void checkAlgTest() { //true if food contains any allergies (i.e. allergy arraylist is not empty), false otherwise 
+		ArrayList<String> allerg = new ArrayList<String>();
+		allerg.add("peanut");
+		FoodObject food = new FoodObject("Peanut Butter", "snack", 180, allerg);
+		FoodObject food2 = new FoodObject("Apple", "fruit", 125, new ArrayList<String>());
+
+		assertTrue(food.checkAlg());
+		assertFalse(food2.checkAlg());
+	}
+	
+	@Test
+	public void adjustCalTest(){ //should triple and double the tested objects respectively
+		ArrayList<String> allerg = new ArrayList<String>();
+		allerg.add("peanut");
+		FoodObject food = new FoodObject("Peanut Butter", "snack", 180, allerg);
+		FoodObject food2 = new FoodObject("Apple", "fruit", 125, new ArrayList<String>());
+		food.adjustCal(3);
+		food2.adjustCal(2);
+		
+		assertEquals(540, food.getCal());
+		assertEquals(250, food2.getCal());
+	}
+	
 }
