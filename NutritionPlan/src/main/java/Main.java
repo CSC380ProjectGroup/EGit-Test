e688ec670394b500cf84a1246d63832648c26a35
Brandon <brandon.druschel@gmail.com>
2017-03-09 11:54:26 -0500

    Created Main class and its tester

---
+++ NutritionPlan/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+	//Profile Options
+		Profile p1 = new Profile("Profile 1");
+		Profile p2 = new Profile("Profile 2");
+		Profile p3 = new Profile("Profile 3");
+		
+	//Meal Options
+		Meal b = new Meal("Breakfast");
+		Meal l = new Meal("Lunch");
+		Meal d = new Meal("Dinner");
+		Meal o = new Meal("Other");
+		
+		
+		
+	}
+	
+}
---
+++ NutritionPlan/src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import org.junit.Test;
+
+/**
+* Tester for Main
+*/
+
+public class MainTest {
+
+	
+	
+}
