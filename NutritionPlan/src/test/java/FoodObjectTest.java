84271f15ebb2b3ca54fbbbb713fc16742435e666
Brandon <brandon.druschel@gmail.com>
2017-04-02 15:58:02 -0400

    Optimized FoodObjectTest

--- NutritionPlan/src/test/java/FoodObjectTest.java
+++ NutritionPlan/src/test/java/FoodObjectTest.java
@@ -9,35 +9,32 @@ import org.junit.Test;
 */
 
 public class FoodObjectTest {
+	
+	ArrayList<String> allerg = new ArrayList<String>();
+	FoodObject food = new FoodObject("Peanut Butter", "snack", 180, allerg, 1);
+	FoodObject food2 = new FoodObject("Apple", "fruit", 125, new ArrayList<String>(), 1);
+	FoodObject food3 = new FoodObject("Impossible Fruit", "fruit", -125, new ArrayList<String>(), 1);
 
+	
 	@Test
 	public void checkCalTest() { //true if calories is nonnegative, false otherwise
-		ArrayList<String> allerg = new ArrayList<String>();
 		allerg.add("peanut");
-		FoodObject food = new FoodObject("Peanut Butter", "snack", 180, allerg, 1);
-		FoodObject food2 = new FoodObject("Impossible Fruit", "fruit", -125, new ArrayList<String>(), 1);
-
 		assertTrue(food.checkCal(food.getCal()));
-		assertFalse(food.checkCal(food2.getCal()));
+		assertFalse(food.checkCal(food3.getCal()));
+		allerg.clear();
 	}
 	
 	@Test
 	public void checkAlgTest() { //true if food contains any allergies (i.e. allergy arraylist is not empty), false otherwise 
-		ArrayList<String> allerg = new ArrayList<String>();
 		allerg.add("peanut");
-		FoodObject food = new FoodObject("Peanut Butter", "snack", 180, allerg, 1);
-		FoodObject food2 = new FoodObject("Apple", "fruit", 125, new ArrayList<String>(), 1);
-
 		assertTrue(food.checkAlg());
 		assertFalse(food2.checkAlg());
+		allerg.clear();
 	}
 	
 	@Test
 	public void adjustCalTest(){ //should triple and double the tested objects respectively
-		ArrayList<String> allerg = new ArrayList<String>();
 		allerg.add("peanut");
-		FoodObject food = new FoodObject("Peanut Butter", "snack", 180, allerg, 1);
-		FoodObject food2 = new FoodObject("Apple", "fruit", 125, new ArrayList<String>(), 1);
 //		food.adjustCal(3);
 //		food2.adjustCal(2);
 		food.setQuantity(3);  //setQuantity() runs the adjustCal() method
@@ -45,6 +42,7 @@ public class FoodObjectTest {
 		
 		assertEquals(540, food.getCal());
 		assertEquals(250, food2.getCal());
+		allerg.clear();
 	}
 	
 }
