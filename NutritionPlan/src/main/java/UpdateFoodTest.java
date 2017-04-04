5fca1cc018df84eda447bf3c1c8a14176d12116c
CSC380ProjectGroup <dbesemer@oswego.edu>
2017-04-04 12:59:04 -0400

    test classes for database queries

--- NutritionPlan/src/main/java/UpdateFoodTest.java
+++ NutritionPlan/src/main/java/UpdateFoodTest.java
@@ -2,8 +2,6 @@
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,16 +10,14 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Thomas
+ * @author bolen
  */
-public class UploadFoodTest {
+public class UpdateFoodTest {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        //note: this only works if a NEW food is being added.
-        //if the name of the food is already in the database it will not work
         
         ArrayList<String> a1 = new ArrayList();
         a1.add("appleseeds");
@@ -29,17 +25,19 @@ public class UploadFoodTest {
         ArrayList<String> a2 = new ArrayList();
         a2.add("gluten");
         a2.add("shellfish");
+        a2.add("test");
         FoodObject food2 = new FoodObject("Crab Cakes","seafood",150,a2,1);
         ArrayList<String> a3 = new ArrayList();
-        FoodObject food3 = new FoodObject("Taco","mexican",120,a3,1);
+        a3.add("cheese");
+        FoodObject food3 = new FoodObject("Taco","Mexican",120,a3,2);
         
-        DBStuff.addNewFoodDB(food1);
-        DBStuff.addNewFoodDB(food2);
-        DBStuff.addNewFoodDB(food3);
+        DBStuff.UpdateFoodDB(food1);
+        DBStuff.UpdateFoodDB(food2);
+        DBStuff.UpdateFoodDB(food3);
         
-        System.out.println(DBStuff.getFoodDB(food1.getName()));     
-        System.out.println(DBStuff.getFoodDB(food2.getName())); 
-        System.out.println(DBStuff.getFoodDB(food3.getName())); 
+        System.out.println(DBStuff.getFoodDB(food1.getName()));
+        System.out.println(DBStuff.getFoodDB(food2.getName()));
+        System.out.println(DBStuff.getFoodDB(food3.getName()));
     }
     
 }
