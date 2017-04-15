39676839484528b842c56899d68055fa96d71a60
CSC380ProjectGroup <dbesemer@oswego.edu>
2017-04-15 19:26:49 -0400

    //added methods to control class

--- NutritionPlan/src/main/java/Control.java
+++ NutritionPlan/src/main/java/Control.java
@@ -15,11 +15,44 @@ public class Control {
 	private Profile p1 = new Profile("Profile 1");
 	private Profile p2 = new Profile("Profile 2");
 	private Profile p3 = new Profile("Profile 3");
+        
+        private ArrayList<Profile> profiles = new ArrayList<Profile>();
 	
 	
 	// Sets up all of the objects, to be executed in Main upon running the program
 	public void setup(){
 		meals.addAll(Arrays.asList(breakfast, lunch, dinner, other));
+                for (Profile p : profiles) {
+                    for (Meal meal : meals) {
+                        p.addMeal(meal);
+                    }
+                }
+                profiles.addAll(Arrays.asList(p1, p2, p3));
 	}
+        
+        //copies profile q to profile w where q and w are the names of profiles
+        public void copyProfile(String q, String w){
+            Profile qq = getProfile(q);
+            Profile ww = getProfile(w);
+            ww = qq;
+        }
+        
+        public Profile getProfile(String profileName){
+            Profile pr = null;
+            for (Profile p: profiles) {
+                if (p.getName().equals(profileName)) {
+                    pr = p;
+                }
+            }
+            return pr;
+        }
+        
+        public Meal getMeal(Profile p, String mealName){
+            return p.getMeal(mealName);
+        }
+        
+        //make methods for updating profiles
+        
+
 		
 }
