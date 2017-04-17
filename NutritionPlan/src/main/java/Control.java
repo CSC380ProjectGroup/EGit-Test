7b724dc5baf6699d9cd2d878fedc5af6e14fc942
Brandon <brandon.druschel@gmail.com>
2017-04-17 18:38:35 -0400

    More methods for Control class

--- NutritionPlan/src/main/java/Control.java
+++ NutritionPlan/src/main/java/Control.java
@@ -17,7 +17,7 @@ public class Control {
 	private Profile p2 = new Profile("Profile 2");
 	private Profile p3 = new Profile("Profile 3");
         
-        private ArrayList<Profile> profiles = new ArrayList<Profile>();
+    private ArrayList<Profile> profiles = new ArrayList<Profile>();
 	
 	
 	// Sets up all of the objects, to be executed in Main upon running the program
@@ -31,49 +31,66 @@ public class Control {
                 profiles.addAll(Arrays.asList(p1, p2, p3));
 	}
         
-        //copies profile q to profile w where q and w are the names of profiles
-        public void copyProfile(String q, String w){
-            Profile qq = getProfile(q);
-            Profile ww = getProfile(w);
-            ww = qq;
-        }
-        
-        public Profile getProfile(String profileName){
-            Profile pr = null;
-            for (Profile p: profiles) {
-                if (p.getName().equals(profileName)) {
-                    pr = p;
-                }
+    // Copies profile q to profile w where q and w are the names of profiles
+    public void copyProfile(String q, String w){
+        Profile qq = getProfile(q);
+        Profile ww = getProfile(w);
+        ww = qq;
+    }
+    
+    public Profile getProfile(String profileName){
+        Profile pr = null;
+        for (Profile p: profiles) {
+            if (p.getName().equals(profileName)) {
+                pr = p;
             }
-            return pr;
         }
+        return pr;
+    }
+    
+    public Meal getMeal(Profile p, String mealName){
+        return p.getMeal(mealName);
+    }
+    
+    public void createProfile(String profileName){
+        Profile p = new Profile(profileName);
+    }
+    
+    public Meal createMeal(String type){
+        Meal m = new Meal();
+        m.setType(type);
+        return m;
+    }
+    
+    public void addFoodToMeal(Meal m, String foodName) throws ClassNotFoundException, SQLException{
+        FoodObject f = DBStuff.getFoodDB(foodName);
+        m.addFood(f);
+    }
+    
+    public void addMeal(Profile p, String type){
+        Meal m = createMeal(type);
+        p.addMeal(m);
+    }
         
-        public Meal getMeal(Profile p, String mealName){
-            return p.getMeal(mealName);
-        }
-        
-        public void createProfile(String profileName){
-            Profile p = new Profile(profileName);
-        }
-        
-        public Meal createMeal(String type){
-            Meal m = new Meal();
-            m.setType(type);
-            return m;
-        }
-        
-        public void addFoodToMeal(Meal m, String foodName) throws ClassNotFoundException, SQLException{
-            FoodObject f = DBStuff.getFoodDB(foodName);
-            m.addFood(f);
-        }
-        
-        public void addMeal(Profile p, String type){
-            Meal m = createMeal(type);
-            p.addMeal(m);
-        }
-        
-        
-        
+    public int getMealCal(Meal m){
+    	return m.getTotalCalories();
+    }
+    
+	public String getMealAlg(Meal m){
+    	return m.getAllAlg();
+    }
+	
+	public String getMealFood(Meal m){
+		return m.giveFood();
+	}
 
-		
+	public String getMealNotes(Meal m){
+		return m.getNotes();
+	}
+	
+	public void setMealNotes(Meal m, String n){
+		m.setNotes(n);
+	}
+	
+	
 }
